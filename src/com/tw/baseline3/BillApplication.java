183cca1843b10c797e1e2ad5ff7e9786b131fb6b
karthika <karthika.vsdvn@gmail.com>
2015-09-10 11:06:12 +0530

    Added job of the class for bill application

--- src/com/tw/baseline3/BillApplication.java
+++ src/com/tw/baseline3/BillApplication.java
@@ -1,5 +1,6 @@
 package com.tw.baseline3;
 
+//Bill Application starts the Application and takes input from the user.
 public class BillApplication {
     public String start() {
         return "Bill";
